6cf105df4da5c9810e5b4c78e9cbbaf913b9da33
Rafael Kasinski <rakasin@gmail.com>
2022-10-24 17:18:55 -0300

    Finished JS logic

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -39,6 +39,8 @@ const quizData = [
   }
 ]
 
+const quiz = document.getElementById("quiz")
+const answersEls = document.querySelectorAll(".answer")
 const questionEl = document.getElementById("question")
 const a_text = document.getElementById("a_text")
 const b_text = document.getElementById("b_text")
@@ -47,10 +49,13 @@ const d_text = document.getElementById("d_text")
 const submitBtn = document.getElementById("submit")
 
 let currentQuiz = 0
+let score = 0
 
 loadQuiz()
 
 function loadQuiz() {
+  deselectAnswers()
+
   const currentQuizData = quizData[currentQuiz]
 
   questionEl.innerText = currentQuizData.question
@@ -60,12 +65,40 @@ function loadQuiz() {
   d_text.innerText = currentQuizData.d
 }
 
+function getSelected() {
+  let answer = undefined
+
+  answersEls.forEach ((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id
+    }
+  })
+
+  return answer
+}
+
+function deselectAnswers() {
+  answersEls.forEach ((answerEl) => {
+    answerEl.checked = false
+  })
+}
+
 submitBtn.addEventListener("click", () => {
-  currentQuiz++
-  if (currentQuiz < quizData.length) {
-    loadQuiz()
-  } else {
-    // TODO: Show results
-    alert("You done, bruh! 🍸")
+  // check to see the answer
+  const answer = getSelected()
+
+  console.log(answer)
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++
+    }
+
+      currentQuiz++
+      if (currentQuiz < quizData.length) {
+        loadQuiz()
+      } else {
+        quiz.innerHTML = `<h2>You correctly answered ${score}/${quizData.length} questions!</h2>`
+      }
   }
 })
