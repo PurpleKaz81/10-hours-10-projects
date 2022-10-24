b5702c21a38cba255d793653db8e13f679c681d3
Rafael Kasinski <rakasin@gmail.com>
2022-10-24 12:13:32 -0300

    Did JS objects for questions and answers

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Ten JS projects done in ten hours
 
 Timer done with help from https://webdesign.tutsplus.com/tutorials/how-to-build-a-javascript-countdown-timer--cms-93144
 
+Quiz done with the following design: https://uidesigndaily.fra1.digitaloceanspaces.com/uploads/924/day_924.png
---
+++ quiz-app/script.js
@@ -0,0 +1,40 @@
+// object which will have array of quizzes with question and four answers and correct answer
+
+const quizData = [
+  {
+    question: "How many times did Liz Taylor marry?",
+    a: "5",
+    b: "A gazillion",
+    c: "8",
+    d: "Who cares?",
+    correct: "c"
+  }, {
+    question: "How much does you mum weigh?",
+    a: "She's so fat she's got her own gravitational pull",
+    b: "4.5 kg",
+    c: "Pure light has no weight 🫶",
+    d: "I was born from a jackal",
+    correct: "c"
+  }, {
+    question: "According to Americans, what is the capital of Brazil?",
+    a: "Rio de Janeiro",
+    b: "Buenos Aires",
+    c: "What is Brazil? Isn't it in Africa?",
+    d: "A mixture of the above",
+    correct: "d"
+  }, {
+    question: "What is the greatest band of all time?",
+    a: "King Crimson",
+    b: "My band from high school. We did death metal covers of early Daiana Ross",
+    c: "Ivo Papasov's Bulgarian Wedding Band",
+    d: "I only listen to algorithmic music. That's why I'm single",
+    correct: "a"
+  }, {
+    question: "Who wants to live forever?",
+    a: "No one",
+    b: "Everyone",
+    c: "Connor MacLeod of the clan MacLeod",
+    d: "🎶Ooooo-oooo-ooooo🎶",
+    correct: "d"
+  }
+]
